f278977ccfd46512d37552cd8f9fb728d5ef681b
mangytangy <105401762+mangytangy@users.noreply.github.com>
2022-08-02 19:17:06 -0400

    created player movement

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+
+class player:
+    def __init__(self,img):
+        self.img = img;
+        self.x = 0
+        self.y = 0
+        self.pos = [self.x,self.y]
+        
+    def movementhandler(self,keys):
+        if keys[pygame.K_LEFT]:
+            self.pos[0] -= 5
+        if keys[pygame.K_RIGHT]:
+            self.pos[0] += 5
+        if keys[pygame.K_UP]:
+            self.y += 5
+        if keys[pygame.K_DOWN]:
+            self.y -= 5
+    
+
+
+
+
+
--- window.py
+++ window.py
@@ -2,33 +2,34 @@ import pygame
 import os
 import random
 from screeninfo import get_monitors
-
+from main import player
 
 FPS = 165
 WIDTH = [i.width for i in get_monitors()][-1]
 HEIGHT = [i.height for i in get_monitors()][-1]
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Survival Game")
 
-def draw_window():
-    WIN.fill((0,255,0))
 
-    pygame.display.update()
 
+player = player(pygame.image.load("assets/player.png"))
 
 def main():
     second = 0  # Time
 
     while True:
+        keys = pygame.key.get_pressed()
         pygame.time.Clock().tick(FPS)  # Caps framerate
         second += 1 / FPS  # Keeps track of time
-        pos = pygame.mouse.get_pos()
-        print(pos)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 raise SystemExit
 
-        draw_window()
+        WIN.fill((255,255,255))
+        WIN.blit(player.img,player.pos)
+        player.movementhandler(keys)
+
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
